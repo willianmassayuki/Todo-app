edc3e83b6886ef8258d397e818970ec2d377d115
Willian Sato <willianmassayukisato@gmail.com>
2023-07-03 14:57:07 -0300

    Separação de categorias

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -5,6 +5,9 @@ const AddItem = () => {
   const [inputValue, setInputValue] = useState('');
   const [items, setItems] = useState([]);
   const [status, setStatus] = useState(false);
+  const [clickIndex, setClickIndex] = useState();
+  const [completed, setCompleted] = useState([]); 
+  const [active, setActive] = useState([]);   
 
   // Recebe o valor do input
   function handleInputChange(event) {
@@ -19,30 +22,50 @@ const AddItem = () => {
   }
 
   // Muda o status 
-  function handleClick() {
+  function handleClick(index) {
+    setClickIndex(index);
     setStatus(status => !status);
-    console.log('mude o status');
+    console.log(clickIndex);
+    console.log(status);
+
+  }
+
+
+  // Deleta o item
+  function deleteItem(alvo) {
+    const updatedItems = items.filter(item => item !== alvo);
+    setItems(updatedItems);
+  }
+
+  // Deleta todos os que estão completos
+  function clearCompleted() {
+    
   }
 
   return (
-    <div>
-      <input type="checkbox" onChange={handleChange} />
-      <input
-        type="text"
-        name="entrada"
-        placeholder="Create a new Todo"
-        value={inputValue}
-        onChange={handleInputChange}
-      />
-      
-        {items.map((item, index) => (
-            <div className='item' key={index}>
-                <input type="checkbox" name={`${item+index}`} onClick={handleClick} />
-                <label htmlFor={`${item+index}`} className={`${ status ? 'completed' : ''}`} key={index}>{item}</label>
-            </div>
-        ))}
-     
-    </div>
+    <><div>
+          <input type="checkbox" onChange={handleChange} />
+          <input
+              type="text"
+              name="entrada"
+              placeholder="Create a new Todo"
+              value={inputValue}
+              onChange={handleInputChange} />
+
+          {items.map((item, index) => (
+              <div className='item' key={index}>
+                  <input type="checkbox" name={`${item + index}`} onClick={() => handleClick(index)} />
+                  <label htmlFor={`${item + index}`} className={`${clickIndex === index ? status ? 'completed' : '' : null}`}>{item}</label>
+                  <button onClick={() => deleteItem(item)}><img src="images/icon-cross.svg" alt="" /></button>
+              </div>
+          ))}
+
+        </div>
+        <div className="appFooter">
+            {items.length != 0 ? <p>{items.length} Item{items.length > 1? 's' : ''} left</p> : null}
+
+            <button onClick={clearCompleted}>Clear completed</button>
+        </div></>
   );
 };
 
